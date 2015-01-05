ba23e4a92f9fbbd5b2f1e3c87b4ff92e3dd4249c
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2015-01-05 05:07:01 +0000

    add gitconfig

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="Xmodmap Xresources stumpwmrc tmux.conf xinitrc zshrc xprofile hgrc gdbinit gemrc"
+files="Xmodmap Xresources stumpwmrc tmux.conf xinitrc zshrc xprofile hgrc gdbinit gemrc gitconfig"
 
 verbose(){
 	echo "$@"
