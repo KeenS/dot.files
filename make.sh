026327b31c13a8abb87ff029fd8163dfe560ab88
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-06-19 09:45:43 +0900

    add alacitty.yml

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-files="Xmodmap Xresources stumpwmrc tmux.conf xinitrc zshrc xprofile hgrc gdbinit gdb-dashboard gemrc gitconfig keysnail.js"
+files="Xmodmap Xresources alacritty.yml stumpwmrc tmux.conf xinitrc zshrc xprofile hgrc gdbinit gdb-dashboard gemrc gitconfig keysnail.js"
 
 verbose(){
 	echo "$@"
