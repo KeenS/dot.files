afb3a246b7e66e524d93925d2f4c3b1fabb5e536
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-06-19 09:31:29 +0900

    rename xkremap.rb to xremap.rb and edit some config

---
+++ xremap.rb
@@ -0,0 +1,32 @@
+['slack', 'Thunderbird'].each do |klass |
+  window class_only: 'slack' do
+    remap 'C-b', to: 'Left'
+    remap 'C-f', to: 'Right'
+    remap 'C-p', to: 'Up'
+    remap 'C-n', to: 'Down'
+
+    remap 'M-b', to: 'Ctrl-Left'
+    remap 'M-f', to: 'Ctrl-Right'
+
+    remap 'C-a', to: 'Home'
+    remap 'C-e', to: 'End'
+
+    remap 'C-k', to: ['Shift-End', 'Ctrl-x']
+
+    remap 'C-h', to: 'BackSpace'
+    remap 'C-d', to: 'Delete'
+    remap 'M-d', to: 'Ctrl-Delete'
+
+    remap 'C-w', to: ['Ctrl-Shift-Left', 'Ctrl-x']
+  end
+end
+
+window class_only: 'slack' do
+    remap 'C-m', to: 'Shift-KP_Enter'
+    remap 'C-o', to: ['Shift-KP_Enter', 'Left']
+end
+
+window class_only: 'thunderbird' do
+    remap 'C-m', to: 'KP_Enter'
+    remap 'C-o', to: ['KP_Enter', 'Left']
+end
