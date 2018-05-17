ffe7e67045f0e1f1a76ad1e5d0e6d113cc5f1c95
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2018-05-17 20:47:10 +0900

    tune for me

--- xkeysnail.py
+++ xkeysnail.py
@@ -1,15 +1,39 @@
+# -*- coding: utf-8 -*-
+
 import re
 from xkeysnail.transform import *
 
+# [Global modemap] Change modifier keys as in xmodmap
+define_modmap({
+    Key.CAPSLOCK: Key.LEFT_CTRL
+})
+
+# # [Conditional modmap] Change modifier keys in certain applications
+# define_conditional_modmap(re.compile(r'Emacs'), {
+#     Key.RIGHT_CTRL: Key.ESC,
+# })
+
+# # [Multipurpose modmap] Give a key two meanings. A normal key when pressed and
+# # released, and a modifier key when held down with another key. See Xcape,
+# # Carabiner and caps2esc for ideas and concept.
+# define_multipurpose_modmap(
+#     # Enter is enter when pressed and released. Control when held down.
+#     {Key.ENTER: [Key.ENTER, Key.RIGHT_CTRL]}
+
+#     # Capslock is escape when pressed and released. Control when held down.
+#     # {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
+#     # To use this example, you can't remap capslock with define_modmap.
+# )
+
+
 # Keybindings for Firefox/Chrome
 define_keymap(re.compile("Firefox|Google-chrome"), {
-    # Ctrl+Alt+j/k to switch next/previous tab
-    K("C-M-f"): K("C-TAB"),
-    K("C-M-b"): K("C-Shift-TAB"),
+    K("M-f"): K("C-TAB"),
+    K("M-b"): K("C-Shift-TAB"),
 }, "Firefox and Chrome")
 
 # Emacs-like keybindings in non-Emacs applications
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "gnome-terminal"), {
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-terminal"), {
     # Cursor
     K("C-b"): with_mark(K("left")),
     K("C-f"): with_mark(K("right")),
