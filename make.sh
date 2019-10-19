a82682e9728b03268087072f43d0e5c1166b9ccb
κeen <3han5chou7@gmail.com>
2019-10-19 13:00:43 +0900

    ensure dir exists

--- make.sh
+++ make.sh
@@ -16,6 +16,7 @@ done
 verbose mkdir -p ~/.config/Code/User
 verbose ln -Tsf $(pwd)/settings.json ~/.config/Code/User/settings.json
 
+mkdir -p ~/bin
 cat versions | while read name version; do
     echo "installing $name"
     ./install-${name}.sh $version
