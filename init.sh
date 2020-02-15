ed456ce3e0c0f339b3b54bcc620192a2a25cc375
κeen <3han5chou7@gmail.com>
2020-02-16 01:54:18 +0900

    setup more

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
 LANG=C xdg-user-dirs-gtk-update
 gsettings set org.gnome.desktop.input-sources xkb-options ['ctrl:nocaps']
 
-sudo apt install zsh git curl tmux uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev
+sudo apt install zsh git curl tmux uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools gnome-tweak-tool
 
 sudo pip3 install xkeysnail wakatime
 
+chsh -s /usr/bin/zsh
+
 sudo groupadd -f uinput
 sudo useradd -G input,uinput -s /sbin/nologin xkeysnail
+sudo usermod -aG uinput "$USER"
 sudo tee /etc/udev/rules.d/40-udev-xkeysnail-uinput.rules <<'EOF'
 KERNEL=="uinput", GROUP="uinput"
 EOF
