43f5f343ae1df4a5b3a0d9b09a4dff582e957833
κeen <3han5chou7@gmail.com>
2022-01-30 13:43:44 +0900

    remove xkeysnail setup

--- init.sh
+++ init.sh
@@ -3,26 +3,9 @@ gsettings set org.gnome.desktop.input-sources xkb-options ['ctrl:nocaps']
 
 sudo apt install zsh git curl tmux uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools gnome-tweak-tool keepass2 network-manager-l2tp-gnome xdotool
 
-sudo pip3 install xkeysnail wakatime
+sudo pip3 install wakatime
 
 chsh -s /usr/bin/zsh
 
 sudo groupadd -f uinput
-sudo useradd -G input,uinput -s /sbin/nologin xkeysnail
 sudo usermod -aG uinput "$USER"
-sudo tee /etc/udev/rules.d/40-udev-xkeysnail-uinput.rules <<'EOF'
-KERNEL=="uinput", GROUP="uinput"
-EOF
-
-sudo tee /etc/udev/rules.d/40-udev-xkeysnail-input.rules <<'EOF'
-KERNEL=="event*", NAME="input/%k", MODE="660", GROUP="input"
-EOF
-
-sudo tee /etc/modules-load.d/uinput.conf <<'EOF'
-uinput
-EOF
-
-sudo tee /etc/sudoers.d/10-installer <<EOF
-$USER ALL=(ALL) ALL,\
-         (xkeysnail) NOPASSWD: /usr/local/bin/xkeysnail
-EOF
