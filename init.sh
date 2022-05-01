e41a51b9578271f6d8e3343299aa6fbcb8dbd9d6
κeen <3han5chou7@gmail.com>
2022-05-01 16:27:45 +0900

    manage some GUI apps via snap

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ gsettings set org.gnome.desktop.input-sources xkb-options ['ctrl:nocaps']
 sudo apt install zsh git curl uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools gnome-tweak-tool network-manager-l2tp-gnome xdotool exa ripgrep fd-find bat
 
 sudo pip3 install wakatime
+sudo snap install bitwarden blender discord gimp inkscape obs-studio slack spotify
 
 chsh -s /usr/bin/zsh
 
