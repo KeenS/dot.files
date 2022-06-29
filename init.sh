4c8f6b3f32730f7a81c33c9f443ac587fe874c58
κeen <3han5chou7@gmail.com>
2022-06-30 08:36:17 +0900

    manage inkscape by ppa

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 LANG=C xdg-user-dirs-gtk-update
 gsettings set org.gnome.desktop.input-sources xkb-options ['ctrl:nocaps']
 
-sudo apt install zsh git curl uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools gnome-tweak-tool network-manager-l2tp-gnome xdotool exa ripgrep fd-find bat
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt install zsh git curl uim-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools gnome-tweak-tool network-manager-l2tp-gnome xdotool exa ripgrep fd-find bat inkscape
 
 sudo pip3 install wakatime
-sudo snap install bitwarden blender discord gimp inkscape obs-studio slack spotify
+sudo snap install bitwarden blender discord gimp obs-studio slack spotify
 
 chsh -s /usr/bin/zsh
 
