1afe8e82c5fd3a6cf0e7dc11d4df7d36cb2e3c1c
κeen <3han5chou7@gmail.com>
2024-04-25 11:54:59 +0900

    move helix to apt

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 sudo add-apt-repository ppa:inkscape.dev/stable
 sudo add-apt-repository ppa:maveonair/helix-editor
 
-sudo apt install zsh git curl fctix5 fctix5-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libtree-sitter-dev libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools xdotool exa ripgrep fd-find bat yubico-piv-tool yubioath-desktop scdaemon inkscape
-sudo snap install bitwarden blender discord gimp obs-studio slack spotify helix
+sudo apt install zsh git curl fctix5 fctix5-skk skkdic skkdic-extra python3-pip build-essential cmake pkg-config libtree-sitter-dev libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev autoconf texinfo libgif-dev libtiff-dev libjpeg-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev libtinfo-dev kakasi libcurl4-openssl-dev skktools xdotool exa ripgrep fd-find bat yubico-piv-tool yubioath-desktop scdaemon inkscape helix
+sudo snap install bitwarden blender discord gimp obs-studio slack spotify
 
 chsh -s /usr/bin/zsh
 
